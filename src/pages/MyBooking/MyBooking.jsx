2e48879bbb307c5030d00b12b8e481a71a6ce78a
emon1995 <emonhossain995@gmail.com>
2023-05-11 23:41:34 +0600

    confirm & delete added

--- src/pages/MyBooking/MyBooking.jsx
+++ src/pages/MyBooking/MyBooking.jsx
@@ -1,9 +1,117 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import MyBookingRow from "./MyBookingRow";
+import Swal from "sweetalert2";
 
 const MyBooking = () => {
+  const { user } = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+              const reaming = bookings.filter((booking) => booking._id !== id);
+              setBookings(reaming);
+            }
+          });
+      }
+    });
+  };
+
+  const handleClickConfirm = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          const reaming = bookings.filter((booking) => booking._id !== id);
+          const updated = bookings.find((booking) => booking._id === id);
+          updated.status = "confirm";
+          const newBooking = [updated, ...reaming];
+          setBookings(newBooking);
+        }
+      });
+  };
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/bookings?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setBookings(data);
+      });
+  }, []);
+
   return (
     <div className="my-20">
-      <h1>my booking</h1>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Image</th>
+              <th>Service Name</th>
+              <th>Email</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {bookings.map((booking) => (
+              <MyBookingRow
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+                handleClickConfirm={handleClickConfirm}
+              ></MyBookingRow>
+            ))}
+          </tbody>
+          {/* foot */}
+          <tfoot>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Image</th>
+              <th>Service Name</th>
+              <th>Email</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Status</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
 };
